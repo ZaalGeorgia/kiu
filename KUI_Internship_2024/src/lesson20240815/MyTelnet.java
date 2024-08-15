1fd6ccb56bac1b65f2a798124ef53da5e1495b0b
Zaal <zaal.lianov@gmail.com>
2024-08-15 14:02:23 +0400

    Lesson 2024-08-15: thread & networking (start)

---
+++ KUI_Internship_2024/src/lesson20240815/MyTelnet.java
@@ -0,0 +1,66 @@
+package lesson20240815;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class MyTelnet {
+
+	private static Thread thread;
+
+	public static void main(String[] args) {
+		if (args.length < 2 || args.length > 2) {
+			System.err.println("wrong number of parameters:\nUsage: MyTelnet <host> <port>");
+			System.exit(1);
+		}
+		int port = 0;
+		try {
+			port = Integer.parseInt(args[1]);
+		} catch (Exception e) {
+			System.err.println("wrong port format");
+			System.exit(2);
+		}
+
+		try (Socket socket = new Socket(args[0], port)) {
+			System.out.println(socket);
+			processSocketOutput(socket);
+			processSocketInput(socket);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void processSocketOutput(Socket socket) {
+		try {
+			PrintWriter printer = new PrintWriter(socket.getOutputStream());
+			Scanner scanner = new Scanner(System.in);
+			thread = new Thread(() -> {
+				System.out.println("enter command:");
+				while (scanner.hasNextLine()) {
+					String line = scanner.nextLine();
+					printer.println(line);
+					printer.flush();
+				}
+			});
+			thread.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void processSocketInput(Socket socket) {
+		try (Scanner scanner = new Scanner(socket.getInputStream())) {
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
